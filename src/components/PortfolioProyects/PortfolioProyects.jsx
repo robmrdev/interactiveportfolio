513196a2586304675d95ff6260c4b6e8bcc9952f
Robert Mendoza <robmrivas@gmail.com>
2023-12-07 16:26:56 -0300

    timeline almost done

--- src/components/PortfolioProyects/PortfolioProyects.jsx
+++ src/components/PortfolioProyects/PortfolioProyects.jsx
@@ -1,4 +1,4 @@
-import {useEffect } from 'react';
+import { useEffect } from 'react';
 import './PortfolioProyects.css'
 import gsap from 'gsap';
 import { useRef } from 'react';
@@ -10,14 +10,13 @@ gsap.registerPlugin(MotionPathPlugin)
 
 const PortfolioProyects = () => {
 
-    const containerRef = useRef(null)
-    const boxRef = useRef(null);
-    const right1 = useRef(null);
-    const right2 = useRef(null);
-    const right3 = useRef(null);
-    const right4 = useRef(null);
-    const right5 = useRef(null);
-    const right6 = useRef(null);
+  const containerRef = useRef(null)
+  const boxRef = useRef(null);
+  const right1 = useRef(null);
+  const right2 = useRef(null);
+  const right3 = useRef(null);
+  const right4 = useRef(null);
+  const right5 = useRef(null);
 
   useEffect(() => {
     const container = containerRef.current
@@ -27,63 +26,76 @@ const PortfolioProyects = () => {
     const right31 = right3.current
     const right41 = right4.current
     const right51 = right5.current
-    const right61 = right6.current
     const path = [
-      { x: '5vw', y: '0vh' },
-      { x: '45vw', y: '30vh' },
-      { x: '25vw', y: '60vh' },
-      { x: '45vw', y: '90vh' },
-      { x: '25vw', y: '120vh' },
-    //   { x: '45vw', y: '100vh' },
-    //   { x: '25vw', y: '120vh' }
+      { x: '45vw', y: '25vh' },
+      { x: '25vw', y: '70vh' },
+      { x: '45vw', y: '110vh' },
+      { x: '25vw', y: '150vh' },
+      { x: '45vw', y: '190vh' },
+      // { x: '25vw', y: '220vh' },
+      // { x: '45vw', y: '230vh' }
     ];
 
     gsap.to(element, {
-      duration: 10, // Ajusta según sea necesario
+      duration: 5,
       motionPath: {
         path: path,
         align: element,
         autoRotate: true,
+        // pin: true
       },
-      ease: 'linear',
+      ease: 'power1.inOut',
       scrollTrigger: {
         trigger: container,
-        start: 'top 60%',
-        end: '70% 40%',
-        scrub: 5,
-        markers: true
-        // pin: true
+        start: '-10% 60%',
+        end: '85% 60%',
+        scrub: true,
+        // markers: true,
       },
     });
-    const tl = gsap.timeline({scrollTrigger: {
+    const tl = gsap.timeline({
+      scrollTrigger: {
         trigger: container,
-        start: 'top 60%',
-        end: '70% 40%',
-        scrub: 5,
-        markers: true
-        // pin: true
-      }})
-    tl.fromTo(right11, {x: '110vw'}, {x:'60vw'})
-    tl.fromTo(right21, {x: '110vw'}, {x:'60vw'})
-    tl.fromTo(right31, {x: '110vw'}, {x:'60vw'})
-    tl.fromTo(right41, {x: '110vw'}, {x:'60vw'})
-    tl.fromTo(right51, {x: '110vw'}, {x:'60vw'})
-    tl.fromTo(right61, {x: '110vw'}, {x:'60vw'})
+        start: 'top 50%',
+        end: '75% 50%',
+        scrub: 2,
+        // markers: true,
+      }
+    })
+    tl.fromTo(right11, { x: '110vw' }, { x: '60vw' })
+    tl.fromTo(right21, { x: '110vw' }, { x: '50vw' })
+    tl.fromTo(right31, { x: '110vw' }, { x: '60vw' })
+    tl.fromTo(right41, { x: '110vw' }, { x: '50vw' })
+    tl.fromTo(right51, { x: '110vw' }, { x: '60vw' })
   }, []);
 
-    return (
-        <div className='portfolioProyectsWrapper'>
-            <div className='portfolioProyectsContainer' ref={containerRef}>
-                <div className='box' id='boxId' ref={boxRef}></div>
-                <div className='rightEl' ref={right1}></div>
-                <div className='rightEl' ref={right2}></div>
-                <div className='rightEl' ref={right3}></div>
-                <div className='rightEl' ref={right4}></div>
-                <div className='rightEl' ref={right5}></div>
-                <div className='rightEl' ref={right6}></div>
-            </div>
+
+
+
+  return (
+    <div className='portfolioProyectsWrapper'>
+      <div className='portfolioProyectsContainer' ref={containerRef}>
+        <img src='./airplane.png' className='box' id='boxId' ref={boxRef} />
+        <div className='proyectList'>
+          <div className='rightEl' ref={right1} >
+            <img src='./uku.jpg' className=' breathAnimationY animation1' />
+          </div>
+          <div className='rightEl' ref={right2}>
+            <img src='./quiz.jpg' className='breathAnimationY animation4' />
+          </div>
+          <div className='rightEl' ref={right3}>
+            <img src='./fakeIkea.jpg' className='breathAnimationY animation2' />
+          </div>
+          <div className='rightEl' ref={right4}>
+            <img src='./oldPortfolio.jpg' className='breathAnimationY animation3' />
+          </div>
+          <div className='rightEl' ref={right5}>
+            <img src='./treeStore.jpg' className='breathAnimationY animation1' />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default PortfolioProyects
